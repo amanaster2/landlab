d0042d9364f3df1eebfc8929703c90102b60df0a
Amanda Manaster <amanaste@uw.edu>
2018-05-31 14:39:07 -0700

    Adding personal KinwaveImplicitOverlandFlow updates to ADM_master

--- landlab/components/overland_flow/__init__.py
+++ landlab/components/overland_flow/__init__.py
@@ -2,8 +2,10 @@ from .generate_overland_flow_Bates import OverlandFlowBates
 from .generate_overland_flow_deAlmeida import OverlandFlow
 from .generate_overland_flow_implicit_kinwave import KinwaveImplicitOverlandFlow
 from .generate_overland_flow_kinwave import KinwaveOverlandFlowModel
+from .generate_overland_flow_implicit_kinwave_ADM import KinwaveImplicitOverlandFlowADM
 
 __all__ = ['OverlandFlowBates',
            'OverlandFlow',
 	   'KinwaveImplicitOverlandFlow',
-       'KinwaveOverlandFlowModel']
+       'KinwaveOverlandFlowModel',
+	   'KinwaveImplicitOverlandFlowADM']
